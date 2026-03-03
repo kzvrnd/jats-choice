6a9852b04ae6a57155afbd22d3eefb118bea3ea7
kzv <kzvrnd@gmail.com>
2026-03-03 12:55:53 -0800

    Removed user logic saving to local file

--- backend/src/controllers/user-controller.js
+++ backend/src/controllers/user-controller.js
@@ -1,34 +1,23 @@
-import fs from 'fs';
+
 import { login, logout } from '../services/auth-service.js';
 
 
 
 export const getUsers = (req, res) => {
-  const users = JSON.parse(fs.readFileSync('users.json', 'utf-8'));
+ 
   return res.status(200).json(users);
 }
 
 export const createUser = (req, res) => {
-    const { username, password } = req.body;
+  const { email, password } = req.body;  
     
-    
-    const data =  fs.readFileSync('users.json', 'utf-8');
-    const userArray = JSON.parse(data);
-  
-    userArray.push({
-      username,
-      password
-    });   
-    
-    fs.writeFileSync('users.json', JSON.stringify(userArray, null, 2));
-    
-    res.send(`Endpoint post working!, ${username}, ${password}`); 
+  res.send(`Endpoint post working!, ${username}, ${password}`); 
 } 
 
 export const loginUser = (req, res) => {
-  login(req, res);
+  res.send(`login endpoint`);
 }
 
 export const logoutUser = (req, res) => {
-  logout(req, res);
+  res.send(`logout endpoint`);
 }
