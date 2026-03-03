b8d7801f7d33dac437f2a6ffe8df906bd2b4d1cc
kzv <kzvrnd@gmail.com>
2026-03-03 10:31:27 -0800

    Add db connection, env vars, seperation of concerns

---
+++ backend/src/config/database.js
@@ -0,0 +1,24 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+
+// load env variables
+dotenv.config();
+
+
+export const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: process.env.DB_STORAGE || "./database.sqlite",
+  logging: false,
+});
+
+export const connectDB = async () => {
+  try{
+    // Just tests the connection does not create tables (sqlite file base so no real connection yet)
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully. ✅');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error); 
+    process.exit(1);
+  }
+}
