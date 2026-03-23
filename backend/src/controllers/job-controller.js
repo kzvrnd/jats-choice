e5b3bdc7b758312e45fed920162c2278ea1b4cc9
kzv <kzvrnd@gmail.com>
2026-03-23 14:54:46 -0700

    Added delete endpoints for jobs

--- backend/src/controllers/job-controller.js
+++ backend/src/controllers/job-controller.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { createJob, getJobsByUser } from '../services/job-service.js';
+import * as jobService from '../services/job-service.js';
+//import { createJob, getJobsByUser } from '../services/job-service.js';
 
 export const addJob = async (req, res) => {  
 
   try {
-    const job = await createJob(req.user.id, req.body);
+    const job = await jobService.createJob(req.user.id, req.body);
     return res.status(201).json({ message: "Job created successfully", job: job });
   } catch (error) {
     //console.log(error);
@@ -16,10 +17,24 @@ export const addJob = async (req, res) => {
 
 export const getJobs = async (req, res) => {
   try {
-    const jobs = await getJobsByUser(req.user.id);
+    const jobs = await jobService.getJobsByUser(req.user.id);
     return res.status(200).json({ jobs: jobs });
   } catch (error) {
     //console.log(error);
     return res.status(400).json({ message: error.message});
   }
 }
+
+export const deleteJob = async (req, res) => { 
+
+  try {
+    await jobService.deleteJob(req.user.id, req.params.id);
+    //return res.status(200).json({ message: "Job deleted successfully"});
+    return res.status(204).send();
+
+  } catch (error) {
+    //console.log(error);
+    return res.status(error.statusCode || 500).json({ message: error.message || "Sever error deleting job"});
+  }
+}  
+
--- backend/src/routes/job-routes.js
+++ backend/src/routes/job-routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { addJob, getJobs } from '../controllers/job-controller.js';
+import { addJob, getJobs, deleteJob } from '../controllers/job-controller.js';
 import { authenticate } from '../middleware/auth-middleware.js';
 
 const router = express.Router();
 
 router.post('/createjob', authenticate, addJob); // user adds a job
 router.get('/getjobs', authenticate, getJobs);
+router.delete('/:id', authenticate, deleteJob);
 
 
 export default router
